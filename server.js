74d4267ba4d4f2f7c67e92a6509f654de497cde5
successxx <kyle@epub.ai>
2024-08-19 16:23:47 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 app.use(cors());
 
 let latestText = '';
